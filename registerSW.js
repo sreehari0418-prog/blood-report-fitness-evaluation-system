ffa02f900f614a09a3db3b5d8b329d0fb1c42f95
Roli <roli@Rolis-MacBook-Air.local>
2026-02-03 20:10:25 +0530

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/blood-report-fitness-evaluation-system/sw.js', { scope: '/blood-report-fitness-evaluation-system/' })})}
